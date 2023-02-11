36543c473485741e7390ec4be98c3ac4bfa40e25
Emoses95 <moses.e.collins@gmail.com>
2023-02-11 00:17:30 -0800

    prompts for user input

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-// TODO: Include packages needed for this application
+const inquirer= require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ([
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter description of the project',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instructions',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidlines',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email.',
+      },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
